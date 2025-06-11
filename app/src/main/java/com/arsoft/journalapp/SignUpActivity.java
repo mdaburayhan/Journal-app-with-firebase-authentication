37ccaf5b4c6c5f3d3c25559bd5c0205e3439c312
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-06-11 11:52:01 +0600

    added authStateListener to check the user logged in or not

--- app/src/main/java/com/arsoft/journalapp/SignUpActivity.java
+++ app/src/main/java/com/arsoft/journalapp/SignUpActivity.java
@@ -1,24 +1,71 @@
 package com.arsoft.journalapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    // Widgets
+    EditText password_create,username_create, email_create;
+    Button createBTN;
+
+
+    // Firebase Auth
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    // Firebase Connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        createBTN = findViewById(R.id.acc_signUp_btn);
+        password_create = findViewById(R.id.password_create);
+        email_create = findViewById(R.id.email_create);
+        username_create = findViewById(R.id.username_create_ET);
+
+
+        // Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // Listening for changes in the authentication
+        // state and responds accordingly when the
+        // state changes
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                // Check the user if logged in or not
+                if (currentUser != null){
+                    // user Already logged in
+                }else {
+                    // the user signed out
+                }
+
+            }
+        }
+
     }
 }
