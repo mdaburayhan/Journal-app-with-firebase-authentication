d3967b0b736746159f774f486bc1f83bd1cbb48e
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-06-11 18:49:33 +0600

    New Functionalities - New Menu - Sign Out - addJournalActivity

--- app/src/main/java/com/arsoft/journalapp/JournalListActivity.java
+++ app/src/main/java/com/arsoft/journalapp/JournalListActivity.java
@@ -1,14 +1,52 @@
 package com.arsoft.journalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JournalListActivity extends AppCompatActivity {
+
+    // FirebaseAuth
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    // Firebase Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference =
+            db.collection("Journal");
+
+
+    // Firebase Storage
+    private StorageReference storageReference;
+
+    // List of Journals
+    private List<Journal> journalList;
+
+    // RecyclerView
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +54,51 @@ public class JournalListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_journal_list);
 
 
+        // Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        // Widgets
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Posts Arraylist
+        journalList = new ArrayList<>();
+
+
+
+    }
+
+    // 2- adding a menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.action_add) {
+            if (user != null && firebaseAuth != null) {
+                Intent i = new Intent(JournalListActivity.this,
+                        AddJournalActivity.class);
+                startActivity(i);
+            }
+        }else if(itemId == R.id.action_signout){
+            if (user != null && firebaseAuth != null){
+                firebaseAuth.signOut();
+                Intent i = new Intent(JournalListActivity.this,
+                        MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+                finish();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
