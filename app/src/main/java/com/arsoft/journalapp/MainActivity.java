73b0a37e0810b302511d7533a6c5c53ddf4cbf74
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-06-11 12:40:49 +0600

    adding functionality for log in with email and password

--- app/src/main/java/com/arsoft/journalapp/MainActivity.java
+++ app/src/main/java/com/arsoft/journalapp/MainActivity.java
@@ -2,21 +2,39 @@ package com.arsoft.journalapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     // Widgets
     Button loginBtn, createAccountBtn;
     private EditText emailEt, passEt;
+
+    // Firebase Auth
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +50,57 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        // Login
+        loginBtn = findViewById(R.id.email_signin);
+        emailEt = findViewById(R.id.email);
+        passEt = findViewById(R.id.password);
+
+        // Firebase Authentication
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(v -> {
+
+            loginEmailPassUser(
+                    emailEt.getText().toString().trim(),
+                    passEt.getText().toString().trim()
+            );
+
+
+        });
+
+
     }
+
+
+    private void loginEmailPassUser(
+            String email, String pass
+    ){
+        // Checking for empty text
+        if (!TextUtils.isEmpty(email)
+                && !TextUtils.isEmpty(pass)
+        ){
+            firebaseAuth.signInWithEmailAndPassword(
+                    email, pass
+            ).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                    Intent i = new Intent(MainActivity.this, JournalListActivity.class);
+                    startActivity(i);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,
+                            ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+    }
+
+
+
 }
